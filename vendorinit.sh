60d14834511efb3fccc8169fda9b354da1c088c9
trevd <trevd@users.noreply.github.com>
2014-06-12 23:23:32 +0100

    Fix Language Failure

--- vendorinit.sh
+++ vendorinit.sh
@@ -9,14 +9,12 @@ LOCAL_MANIFEST_FILE=$LOCAL_MANIFEST_DIR/$VENDOR_MANIFEST_FILE
 rm -rfv $LOCAL_MANIFESTS_FILE
 mkdir -pv $LOCAL_MANIFESTS_DIR
 if [ ! -z $USE_LOCAL_MIRROR ] ; then
+  echo "Going Loco in Acapulco"
   repo init -u /src/androidarmv6/android.git -b cm-11.0
 else
+	echo "Getting Remote"
   repo init -u git://github.com/androidarmv6/android.git -b cm-11.0
-endif
+fi
 
 
 curl https://raw.github.com/trevd/android_vendor_broadcom_rpi/cm-11.0/manifests/$VENDOR_MANIFEST_FILE > $LOCAL_MANIFEST_FILE
-
-
-
-
