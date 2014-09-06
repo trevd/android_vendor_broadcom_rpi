22244702721e0ce099c0c5da4b11a1ffd08271ae
android <trevd1234@gmail.com>
2014-09-06 01:22:18 +0100

    automatically get cm prebuilts

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
 echo "Patching Build System - Making PI Friendly"
 patch  --silent --forward --reject-file=- --strip=0 < vendor/broadcom/rpi/patches/android_build.patch
+if [ ! -f vendor/cm/proprietary/Term.apk ] ; then
+    ./vendor/cm/get-prebuilts
+fi
